2f7295045d0ee9466638dd0592d11edf62574804
Sander <alex.kapusniak@gmail.com>
2022-01-02 14:49:34 +0200

    add calculator

--- main/java/com/sander/spring/config/MySpringMVCDispatcherServletInitializer.java
+++ main/java/com/sander/spring/config/MySpringMVCDispatcherServletInitializer.java
@@ -2,19 +2,20 @@ package com.sander.spring.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class MySpringMVCDispatcherSevletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MySpringMVCDispatcherServletInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringConfig.class};
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
     }
 }
---
+++ main/java/com/sander/spring/controllers/FirstController.java
@@ -0,0 +1,55 @@
+package com.sander.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "operation") String operation,
+                            Model model) {
+
+        double result;
+
+        switch (operation) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / b;
+                break;
+                default: result = 0;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
+    }
+}
